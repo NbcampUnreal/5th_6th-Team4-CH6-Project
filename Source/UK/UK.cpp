0ca9afbf629e61c121e3fb9e97f38fdd215abdd8
guapo02 <guapo1212@naver.com>
2026-01-22 15:13:45 +0900

    Project Setting

---
+++ Source/UK/UK.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UK.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UK, "UK" );
+
+DEFINE_LOG_CATEGORY(LogUK)
