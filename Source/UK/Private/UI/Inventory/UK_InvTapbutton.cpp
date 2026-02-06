3033abff3523acff4332a1dfba286ad569e0cf93
Give291 <whdydglgk@gmail.com>
2026-02-06 19:23:49 +0900

    02/06 fix InvUI

---
+++ Source/UK/Private/UI/Inventory/UK_InvTapbutton.cpp
@@ -0,0 +1,27 @@
+﻿#include "UI/Inventory/UK_InvTapbutton.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+
+void UUK_InvTapbutton::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	if (ButtonText)
+	{
+		ButtonText->SetText(InText);
+	}
+}
+
+void UUK_InvTapbutton::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (ButtonTap)
+	{
+		ButtonTap->OnClicked.AddDynamic(this, &UUK_InvTapbutton::ButtonTapClicked);
+	}
+}
+
+void UUK_InvTapbutton::ButtonTapClicked()
+{
+	OnButtonTap.Broadcast(this);
+}
---
+++ Source/UK/Public/UI/Inventory/UK_InvSlot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UK_InvSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UK_API UUK_InvSlot : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
