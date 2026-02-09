30d61547e2afb49c74b03763cb80a45980f48b27
Give291 <whdydglgk@gmail.com>
2026-02-09 10:38:48 +0900

    02/09 fix 인벤토리 UI 디자인

---
+++ Source/UK/Private/UI/Inventory/UK_CategoryTap.cpp
@@ -0,0 +1,17 @@
+﻿#include "UI/Inventory/UK_CategoryTap.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+
+void UUK_CategoryTap::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (CategoryButton)
+	{
+		CategoryButton->OnClicked.AddDynamic(this, &UUK_CategoryTap::CategoryTapClicked);
+	}
+}
+
+void UUK_CategoryTap::CategoryTapClicked()
+{
+	OnCategoryTap.Broadcast(this);
+}
