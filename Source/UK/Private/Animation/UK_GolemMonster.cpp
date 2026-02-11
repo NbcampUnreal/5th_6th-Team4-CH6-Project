badd68f78a840da4ae79e3b85317db24bf70cb9c
jungmin001 <jungmin4556@naver.com>
2026-02-11 21:27:44 +0900

    Fix Golem AnimInstance.cpp
    Update
    Girl Character Sword Animation Retargeting
    
    26.02.11 [Suh Jung Min]

--- Source/UK/Private/Animation/UK_GolemMonster.cpp
+++ Source/UK/Private/Animation/UK_GolemMonster.cpp
@@ -1,1 +1,15 @@
 ﻿#include "Animation/UK_GolemMonster.h"
+
+UUK_GolemMonster::UUK_GolemMonster()
+{
+}
+
+void UUK_GolemMonster::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+}
+
+void UUK_GolemMonster::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+}
