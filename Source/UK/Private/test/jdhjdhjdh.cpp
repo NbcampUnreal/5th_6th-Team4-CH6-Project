86e593b2d859b94e9967dcb412eff974da200611
gamewhoim <gamewhoim@naver.com>
2026-02-12 14:24:16 +0900

    test jdh utf 8

---
+++ Source/UK/Private/test/jdhjdhjdh.cpp
@@ -0,0 +1,27 @@
+﻿// 정다훈정다훈
+
+
+#include "test/jdhjdhjdh.h"
+
+// Sets default values
+Ajdhjdhjdh::Ajdhjdhjdh()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Ajdhjdhjdh::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Ajdhjdhjdh::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UK/Public/test/jdhjdhjdh.h
@@ -0,0 +1,26 @@
+﻿//정다훈정다훈
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "jdhjdhjdh.generated.h"
+
+UCLASS()
+class UK_API Ajdhjdhjdh : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	Ajdhjdhjdh();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
