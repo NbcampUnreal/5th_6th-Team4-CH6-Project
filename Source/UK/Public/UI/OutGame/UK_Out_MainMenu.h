4a97f8ee5db870c95e363484462ddc7318bb2f90
SuhyunPark <xjcl2058@gmail.com>
2026-02-12 14:24:39 +0900

    260212
    LAKEHOUSE

--- Source/UK/Public/UI/OutGame/UK_Out_MainMenu.h
+++ Source/UK/Public/UI/OutGame/UK_Out_MainMenu.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "UK_Out_MainMenu.generated.h"
 
+class UButton;
+class UEditableText;
 /**
  * 
  */
@@ -13,5 +15,26 @@ UCLASS()
 class UK_API UUK_Out_MainMenu : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	UUK_Out_MainMenu(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+	void OnPlayButtonClicked();
+
+	UFUNCTION()
+	void OnExitButtonClicked();
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = USTitleWidget, Meta = ( AllowPrivateAccess, BindWidget ))
+	TObjectPtr<UButton> PlayButton;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = USTitleWidget, Meta = ( AllowPrivateAccess, BindWidget ))
+	TObjectPtr<UButton> ExitButton;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = USLobbyLevelUI, Meta = ( AllowPrivateAccess, BindWidget ))
+	TObjectPtr<UEditableText> ServerIPEditableText;
 	
 };
