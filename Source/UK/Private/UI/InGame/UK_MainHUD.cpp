570314617928565f79c09e37019fa6e5bbee2e1c
BK <quannie70@gmail.com>
2026-02-13 19:00:21 +0900

    UI 플레이어, 몬스터 체력바 구현

--- Source/UK/Private/UI/InGame/UK_MainHUD.cpp
+++ Source/UK/Private/UI/InGame/UK_MainHUD.cpp
@@ -1,5 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "UI/InGame/UK_MainHUD.h"
+#include "Components/ProgressBar.h"
+#include "Kismet/GameplayStatics.h"
 
+void UUK_MainHUD::NativeConstruct()
+{
+	Super::NativeConstruct();
 
-#include "UI/InGame/UK_MainHUD.h"
+	// 플레이어 캐릭터 가져오기
+	APawn* PlayerPawn = GetOwningPlayerPawn();
+	if ( PlayerPawn )
+	{
+		// 캐릭터에서 StatusComponent 찾기
+		UStatusComponent* StatusComp = PlayerPawn->FindComponentByClass<UStatusComponent>();
+		if ( StatusComp )
+		{
+			// 3. 델리게이트 바인딩 
+			StatusComp->HpStatusDelegate.AddDynamic(this, &UUK_MainHUD::UpdateHealthBar);
+
+		}
+	}
+}
+
+void UUK_MainHUD::UpdateHealthBar(float CurrentHp, float MaxHp)
+{
+	if ( HealthBar && MaxHp > 0.f )
+	{
+		// 0.0 ~ 1.0 사이의 퍼센트 값으로 변환하여 반영
+		HealthBar->SetPercent(CurrentHp / MaxHp);
+	}
+}
 
