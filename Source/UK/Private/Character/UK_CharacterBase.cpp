2f345c5374236cb91a238eb583f20e321b6d76a0
ParkChanYeong <sidsru21131210@gmail.com>
2026-02-13 14:07:14 +0900

    무기 위치 조정(ing)

--- Source/UK/Private/Character/UK_CharacterBase.cpp
+++ Source/UK/Private/Character/UK_CharacterBase.cpp
@@ -210,130 +210,131 @@ void AUK_CharacterBase::LockON()
 {
 	if ( bIsLock == false )
 	{
-		AUK_PlayerController* UKPC = Cast<AUK_PlayerController>(GetController());
-		if ( IsValid(UKPC) == false )
-			return;
-		FVector Start;
-		FRotator CameraRot;
-		const float CapsuleRadius = 50.f;
-		// 카메라부터 카메라가 보는 방향으로 트레이스 실시
-		UKPC->GetPlayerViewPoint(Start, CameraRot);
-		FVector ForwardVector = CameraRot.Vector();/*카메라의 방향성*/
+		//	AUK_PlayerController* UKPC = Cast<AUK_PlayerController>(GetController());
+		//	if ( IsValid(UKPC) == false )
+		//		return;
+		//	FVector Start;
+		//	FRotator CameraRot;
+		//	const float CapsuleRadius = 50.f;
+		//	// 카메라부터 카메라가 보는 방향으로 트레이스 실시
+		//	UKPC->GetPlayerViewPoint(Start, CameraRot);
+		//	FVector ForwardVector = CameraRot.Vector();/*카메라의 방향성*/
 
-		float TraceDistance = 1000.f;
-		FVector End = Start + ( ForwardVector * TraceDistance );
+		//	float TraceDistance = 1000.f;
+		//	FVector End = Start + ( ForwardVector * TraceDistance );
 
-		FCollisionQueryParams Params;
-		Params.AddIgnoredActor(this);
-		FCollisionShape CollisionShape = FCollisionShape::MakeSphere(CapsuleRadius);
+		//	FCollisionQueryParams Params;
+		//	Params.AddIgnoredActor(this);
+		//	FCollisionShape CollisionShape = FCollisionShape::MakeSphere(CapsuleRadius);
 
-		bool bHit = GetWorld()->SweepMultiByChannel(
-			LockOnResult,
-			Start,
-			End,
-			FQuat::Identity,
-			ECC_LockOn,/*추후에 카메라 전용 트레이스 채널로 변경 요망*/
-			CollisionShape,
-			Params
+		//	bool bHit = GetWorld()->SweepMultiByChannel(
+		//		LockOnResult,
+		//		Start,
+		//		End,
+		//		FQuat::Identity,
+		//		ECC_LockOn,/*추후에 카메라 전용 트레이스 채널로 변경 요망*/
+		//		CollisionShape,
+		//		Params
+		//	);
+		//	FColor DrawColor = bHit ? FColor::Green : FColor::Red;
+
+		//	FQuat CapsuleRot = FRotationMatrix::MakeFromZ(Start - End).ToQuat();
+		//	DrawDebugCapsule(
+		//		GetWorld(),
+		//		( Start + End ) / 2,
+		//		( End - Start ).Size(),
+		//		CapsuleRadius,
+		//		CapsuleRot,
+		//		DrawColor,
+		//		false,
+		//		1.f
+		//	);
+		//	if ( bHit )
+		//	{
+		bIsLock = true;
+		bUseControllerRotationYaw = true;
+		GetCharacterMovement()->bOrientRotationToMovement = false;
+		GetWorld()->GetTimerManager().SetTimer(
+			LockOnTimer,
+			this,
+			&AUK_CharacterBase::LockONTick,
+			0.01f,
+			true
 		);
-		FColor DrawColor = bHit ? FColor::Green : FColor::Red;
 
-		FQuat CapsuleRot = FRotationMatrix::MakeFromZ(Start - End).ToQuat();
-		DrawDebugCapsule(
-			GetWorld(),
-			( Start + End ) / 2,
-			( End - Start ).Size(),
-			CapsuleRadius,
-			CapsuleRot,
-			DrawColor,
-			false,
-			1.f
-		);
-		if ( bHit )
-		{
-			bIsLock = true;
-			GetWorld()->GetTimerManager().SetTimer(
-				LockOnTimer,
-				this,
-				&AUK_CharacterBase::LockONTick,
-				0.01f,
-				true
-			);
-
-		}
+		//}
 	}
-	else
-	{
-		bIsLock = false;
-		GetWorld()->GetTimerManager().ClearTimer(LockOnTimer);
-		LockOnResult.Reset();
-		LockOnTimer.Invalidate();
-	}
+	//else
+	//{
+	//	bIsLock = false;
+	//	GetWorld()->GetTimerManager().ClearTimer(LockOnTimer);
+	//	LockOnList.Reset();
+	//	LockOnTimer.Invalidate();
+	//}
 }
 void AUK_CharacterBase::LockONTick()
 {
-	if ( LockOnResult.IsEmpty() == false )
+	if ( LockOnList.IsEmpty() == false )
 	{
-		int32 size = LockOnResult.Num();
+		int32 size = LockOnList.Num();
 		if ( size <= index )
 		{
 			index = 0;
 		}
-		AActor* Actor = LockOnResult[ index ].GetActor();
-
-		if ( IsValid(Actor) == true )
+		AAIMonsterBase* Monster = LockOnList[ index ];
+		if ( IsValid(Monster) == false )
 		{
-			AAIMonsterBase* Monster = Cast<AAIMonsterBase>(Actor);
-			if ( IsValid(Monster) == false )
+			LockOnList.RemoveAtSwap(index);
+			return;
+		}
+
+		if ( Monster->IsDead() == false )
+		{
+			float Distance = FVector::Dist(GetActorLocation(), Monster->GetActorLocation());
+			if ( Distance > MaxLockDistance )
 			{
-				LockOnResult.RemoveAtSwap(index);
+				bIsLock = false;
+				GetWorld()->GetTimerManager().ClearTimer(LockOnTimer);
+				LockOnList.Reset();
+				LockOnTimer.Invalidate();
+				bUseControllerRotationYaw = false;
+				GetCharacterMovement()->bOrientRotationToMovement = true;
 				return;
 			}
+			const float DeltaTime = GetWorld()->GetDeltaSeconds();
+			FVector Start = GetActorLocation();
+			FVector End = Monster->GetActorLocation();
+			FRotator Target = UKismetMathLibrary::FindLookAtRotation(Start, End);
+			FRotator NowRot = GetController()->GetControlRotation();
+			FRotator NewRot = FMath::RInterpTo(
+				NowRot,
+				Target,
+				DeltaTime,
+				12.f
+			);
 
-			if ( Monster->IsDead() == false )
-			{
-				float Distance = FVector::Dist(GetActorLocation(), Monster->GetActorLocation());
-				if ( Distance > MaxLockDistance )
-				{
-					bIsLock = false;
-					GetWorld()->GetTimerManager().ClearTimer(LockOnTimer);
-					LockOnResult.Reset();
-					LockOnTimer.Invalidate();
-					return;
-				}
-				const float DeltaTime = GetWorld()->GetDeltaSeconds();
-				FVector Start = GetActorLocation();
-				FVector End = Monster->GetActorLocation();
-				FRotator Target = UKismetMathLibrary::FindLookAtRotation(Start, End);
-				FRotator NowRot = GetController()->GetControlRotation();
-				FRotator NewRot = FMath::RInterpTo(
-					NowRot,
-					Target,
-					DeltaTime,
-					12.f
-				);
+			GetController()->SetControlRotation(NewRot);
 
-				GetController()->SetControlRotation(NewRot);
-
-			}
-			else if ( Monster->IsDead() == true )
-			{
-				LockOnResult.RemoveAtSwap(index);
-			}
 		}
-		else
+		else if ( Monster->IsDead() == true )
 		{
-			LockOnResult.RemoveAtSwap(index);
+			LockOnList.RemoveAtSwap(index);
 		}
 	}
 	else
 	{
 		bIsLock = false;
 		GetWorld()->GetTimerManager().ClearTimer(LockOnTimer);
-		LockOnResult.Reset();
+		LockOnList.Reset();
 		LockOnTimer.Invalidate();
+		bUseControllerRotationYaw = false;
+		GetCharacterMovement()->bOrientRotationToMovement = true;
 		return;
 	}
+}
+void AUK_CharacterBase::AddTarget(const TObjectPtr<AAIMonsterBase> Monster)
+{
+	LockOnList.AddUnique(Monster);
 }
 #pragma endregion
 
@@ -349,10 +350,48 @@ void AUK_CharacterBase::EquipWeapon(FGameplayTag NewWeapon)
 	if ( IsValid(Weapon->GetRightHandWeapon()) )
 	{
 		RightHandWeaponComponent->SetSkeletalMesh(Weapon->GetRightHandWeapon()); // todo : 이후에 서버에서 변경하도록 수정해야함 임시로 클라에서만 변경하고 있음
+		const FName WeaponSocketName = TEXT("Weapon_rSocket");
+		const FName GripSocketName = TEXT("GripSocket");
+
+		FTransform GripSocketTransform = RightHandWeaponComponent->GetSocketTransform(
+			GripSocketName,
+			RTS_Component
+		);
+
+		RightHandWeaponComponent->AttachToComponent(
+			GetMesh(),
+			FAttachmentTransformRules::SnapToTargetNotIncludingScale,
+			WeaponSocketName
+		);
+
+		RightHandWeaponComponent->SetRelativeTransform(GripSocketTransform.Inverse());
+	}
+	else
+	{
+		RightHandWeaponComponent->SetSkeletalMesh(nullptr);
 	}
 	if ( IsValid(Weapon->GetLeftHandWeapon()) )
 	{
 		LeftHandWeaponComponent->SetSkeletalMesh(Weapon->GetLeftHandWeapon()); // todo : 이후에 서버에서 변경하도록 수정해야함 임시로 클라에서만 변경하고 있음
+		const FName WeaponSocketName = TEXT("Weapon_rSocket");
+		const FName GripSocketName = TEXT("GripSocket");
+
+		FTransform GripSocketTransform = LeftHandWeaponComponent->GetSocketTransform(
+			GripSocketName,
+			RTS_Component
+		);
+
+		LeftHandWeaponComponent->AttachToComponent(
+			GetMesh(),
+			FAttachmentTransformRules::SnapToTargetNotIncludingScale,
+			WeaponSocketName
+		);
+
+		LeftHandWeaponComponent->SetRelativeTransform(GripSocketTransform.Inverse());
+	}
+	else
+	{
+		LeftHandWeaponComponent->SetSkeletalMesh(nullptr);
 	}
 	AnimationComponent->SetNowWeapon(Weapon);
 	GetAbilitySystemComponent()->AddLooseGameplayTag(NowWeapon);
