e4ceae93acefa65ac90d4d910a011a5f9d70b552
Give291 <whdydglgk@gmail.com>
2026-02-13 14:08:32 +0900

    02/13 인벤토리 info 수정

--- Source/UK/Private/UI/Inventory/UK_InvInfo.cpp
+++ Source/UK/Private/UI/Inventory/UK_InvInfo.cpp
@@ -13,28 +13,24 @@ void UUK_InvInfo::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	//처음에는 안보이게, 툴팁이 슬롯을 막을 경우가 있어서
 	SetVisibility(ESlateVisibility::Hidden);
-	SetIsEnabled(false);
+	SetIsEnabled(true);
 }
 //슬롯에 마우스 올렸을 때 정보 보여주기
 void UUK_InvInfo::SlotMouse(UDataTable* InItemDataTable, const FInventorySlot& InSlot, float OffsetX, float OffsetY)
 {
-	ItemDataTable = InItemDataTable;
-
 	if(!ItemDataTable || InSlot.isEmpty())
 	{
 		HideToolInfo();
 		return;
 	}
 
-	ApplyItemID(InSlot.ItemID);
-
 	FVector2D MousePos;
+	ApplyItemID(InSlot.ItemID);
 
 	const bool bIsMouse = UWidgetLayoutLibrary::GetMousePositionScaledByDPI(GetOwningPlayer(), MousePos.X, MousePos.Y);
 
-	if(!bIsMouse)
+	if (!bIsMouse)
 	{
 		SetVisibility(ESlateVisibility::HitTestInvisible);
 		return;
@@ -56,11 +52,6 @@ void UUK_InvInfo::ClearInfo()
 	if (ItemNameText)ItemNameText->SetText(FText::GetEmpty());
 	if (ItemDescriptionText) ItemDescriptionText->SetText(FText::GetEmpty());
 
-	if (ItemImage)
-	{
-		ItemImage->SetBrushFromTexture(nullptr);
-		ItemImage->SetVisibility(ESlateVisibility::Hidden);
-	}
 	if (ItemTypeImage)
 	{
 		ItemTypeImage->SetBrushFromTexture(nullptr);
@@ -80,13 +71,6 @@ void UUK_InvInfo::ApplyItemID(FName ItemID)
 
 	if (ItemNameText) ItemNameText->SetText(ItemInfo->ItemName);
 	if (ItemDescriptionText) ItemDescriptionText->SetText(ItemInfo->ItemDescription);
-
-	if (ItemImage)
-	{
-		UTexture2D* Icon = ItemInfo->ItemIcon.LoadSynchronous();
-		ItemImage->SetBrushFromTexture(Icon);
-		ItemImage->SetVisibility(Icon ? ESlateVisibility::Visible : ESlateVisibility::Hidden);
-	}
 
 	ApplyItemTag(ItemInfo->ItemTag);
 }
@@ -125,26 +109,6 @@ void UUK_InvInfo::ApplyItemTag(const FGameplayTag& Tag)
 	}
 }
 
-//화면 밖으로 나가지 않도록 위치 설정
 void UUK_InvInfo::SetTooltipPositionClamped(const FVector2D& MousePos, const FVector2D& Offset)
 {
-	//const FVector2D ViewportSize = UWidgetLayoutLibrary::GetViewportSize(this);
-
-	//ForceLayoutPrepass();
-	//const FVector2D TooltipSize = GetDesiredSize();
-
-	//FVector2D Pos = MousePos + Offset;
-
-	////우측/하단 초과하면 반대로 띄우기
-	//if (Pos.X + TooltipSize.X > ViewportSize.X)
-	//	Pos.X = MousePos.X - Offset.X - TooltipSize.X;
-
-	//if (Pos.Y + TooltipSize.Y > ViewportSize.Y)
-	//	Pos.Y = MousePos.Y - Offset.Y - TooltipSize.Y;
-
-	////최소 0 보정
-	//Pos.X = FMath::Max(0.f, Pos.X);
-	//Pos.Y = FMath::Max(0.f, Pos.Y);
-
-	//SetPositionInViewport(Pos, true);
 }
