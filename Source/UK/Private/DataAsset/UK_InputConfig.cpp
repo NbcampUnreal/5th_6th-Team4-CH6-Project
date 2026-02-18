1a75a7fa5e94d65b39753049b5d76b7705a7b762
ParkChanYeong <sidsru21131210@gmail.com>
2026-02-18 17:11:02 +0900

    캐릭터 인풋 수정

---
+++ Source/UK/Private/DataAsset/UK_InputConfig.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataAsset/UK_InputConfig.h"
+
+UInputAction* UUK_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InputTag) const
+{
+	if ( const TObjectPtr <UInputAction>* FoundAction = NativeInputAction.Find(InputTag) )
+	{
+		return *FoundAction;
+	}
+	return nullptr;
+}
