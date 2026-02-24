2230a540ce30c0f3c9620f60989bd07841d6e5d5
SuhyunPark <xjcl2058@gmail.com>
2026-02-24 11:08:43 +0900

    260224

---
+++ Source/UK/Private/UI/InGame/UK_Quest.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/InGame/UK_Quest.h"
+
+UUK_Quest::UUK_Quest(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+void UUK_Quest::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if ( Button_0 )
+	{
+		//Button_0->OnClicked.AddDynamic(this, &ThisClass::);
+	}
+	if ( Button_1 )
+	{
+		//Button_1->OnClicked.AddDynamic(this, &ThisClass::);
+	}
+}
