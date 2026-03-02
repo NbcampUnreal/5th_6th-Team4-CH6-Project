8e359eb02c16714df2b11666dfe19ec97fb389f7
gamewhoim <gamewhoim@naver.com>
2026-03-02 15:34:53 +0900

    severs sessions

--- Source/UK/Public/server/UKGameInstance.h
+++ Source/UK/Public/server/UKGameInstance.h
@@ -8,65 +8,25 @@
 
 
 /* 목록
-1. Lan 기반 세션 구현 및 가능한 오류방지 시스템
+1. 현재 ue 5.7_steam기반 리슨서버는 c++ 네이처로 사용불가하므로, 플러그인 대체함.
+	관련 5.7 fix가 들어오면 c++ 기반 세션구현으로 재작성할것임
 2. savegame 관련추가 내용 (서브시스템 분리시 조정할것, 수정엄금*/
 
-// [1] Lan 기반 세션
+
 
 UCLASS()
 class UK_API UUKGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
-public:
-	virtual void Init() override;
-	virtual void Shutdown() override;
-
-    UFUNCTION(BlueprintCallable)
-    void CreateSessionLAN(int32 PublicConnections = 4);
-
-    UFUNCTION(BlueprintCallable)
-    void FindSessionsLAN(int32 MaxResults = 50);
-
-    UFUNCTION(BlueprintCallable)
-    void JoinFoundSession(int32 Index = 0);
-
-    UFUNCTION(BlueprintCallable)
-    void DestroySessionLAN();
-
-    UFUNCTION(BlueprintCallable)
-    void CloseSessionLAN(); // 유저가 '방 닫기' 누를 때, 재생성 없이 종료만
-
-private:
-    IOnlineSessionPtr SessionInterface;
-    TSharedPtr<FOnlineSessionSearch> SessionSearch;
-
-    bool bDestroyInProgress = false; // Destroy 중 중복 호출 방지
-
-    bool bCreateAfterDestroy = false;  // 유저가 직접 닫을 땐 보통 false.
-    int32 PendingPublicConnections = 4; // 공개슬롯 수. 즉 임시값은 4명방 재생성
-
-    FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
-    FDelegateHandle OnCreateSessionCompleteHandle;
-
-    FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
-    FDelegateHandle OnFindSessionsCompleteHandle;
-
-    FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
-    FDelegateHandle OnJoinSessionCompleteHandle;
-
-    FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
-    FDelegateHandle OnDestroySessionCompleteHandle;
-
-private:
-    void HandleCreateSessionComplete(FName SessionName, bool bWasSuccessful);
-    void HandleFindSessionsComplete(bool bWasSuccessful);
-    void HandleJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
-    void HandleDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+// [1] 세션 구역 _ 현재는 플러그인+bp로 대체중임
 
 // [2] savegame 관련
 
 public:
+	virtual void Init() override;
+	virtual void Shutdown() override;
+
     UFUNCTION(BlueprintCallable)
     bool SaveToSlotSimple(const FString& SlotName = TEXT("UK_SaveSlot"), int32 UserIndex = 0);
 
