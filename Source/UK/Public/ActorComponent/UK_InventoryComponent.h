aede3dc571b24a648908ccdc8a24b83036b4d9e3
Give291 <whdydglgk@gmail.com>
2026-03-05 17:01:46 +0900

    03/05 ItemNotify

--- Source/UK/Public/ActorComponent/UK_InventoryComponent.h
+++ Source/UK/Public/ActorComponent/UK_InventoryComponent.h
@@ -25,6 +25,8 @@ public:
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryUpdate);
+//추가
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnItemAdded, FName, ItemID, int32, Amount);
 
 UCLASS(ClassGroup = ( Custom ), meta = ( BlueprintSpawnableComponent ))
 class UK_API UUK_InventoryComponent : public UActorComponent
@@ -72,6 +74,9 @@ public:
 
 	UPROPERTY(BlueprintAssignable)
 	FOnInventoryUpdate OnInventoryUpdate;
+	//추가
+	UPROPERTY(BlueprintAssignable)
+	FOnItemAdded OnItemAdded;
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
