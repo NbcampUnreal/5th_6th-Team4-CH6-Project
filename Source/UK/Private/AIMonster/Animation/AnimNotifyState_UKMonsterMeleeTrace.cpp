e84739ca1404f62808ead59890f879e689655a9d
p00-h <149079533+p00-h@users.noreply.github.com>
2026-03-06 15:20:07 +0900

    Update AnimNotifyState_UKMonsterMeleeTrace.cpp

--- Source/UK/Private/AIMonster/Animation/AnimNotifyState_UKMonsterMeleeTrace.cpp
+++ Source/UK/Private/AIMonster/Animation/AnimNotifyState_UKMonsterMeleeTrace.cpp
@@ -3,6 +3,7 @@
 #include "Character/UK_CharacterBase.h"
 #include "Kismet/GameplayStatics.h"
 #include "DrawDebugHelpers.h"
+#include "Character/AttibuteSet/UK_PlayerStatusAttributeSet.h"
 #include "GameFramework/Character.h"
 #include "Components/CapsuleComponent.h"
 #include "Sound/SoundCue.h"
@@ -115,8 +116,22 @@ void UAnimNotifyState_UKMonsterMeleeTrace::NotifyTick(
 			*Player->GetName(),
 			Monster->AttackDamage,
 			*Hit.ImpactPoint.ToString());
-
-		Player->ReceiveDamage(Monster->AttackDamage);
+		if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(Player))
+		{
+			if (UAbilitySystemComponent* PlayerASC = ASCInterface->GetAbilitySystemComponent())
+			{
+				const UUK_PlayerStatusAttributeSet* AttrSet = PlayerASC->GetSet<UUK_PlayerStatusAttributeSet>();
+				const float Defence = AttrSet ? AttrSet->GetDefence() : 0.f;
+				const float FinalDamage = FMath::Max(Monster->AttackDamage - Defence, 0.f);
+				if (FinalDamage > 0.f)
+				{
+					UE_LOG(LogTemp, Warning, TEXT("[Player] Hit | Raw: %.1f | Defence: %.1f | Final: %.1f | HP: %.1f -> %.1f"),
+						Monster->AttackDamage, Defence, FinalDamage, AttrSet->GetHealth(), AttrSet->GetHealth() - FinalDamage);
+					PlayerASC->SetNumericAttributeBase(
+						UUK_PlayerStatusAttributeSet::GetDamageAttribute(), FinalDamage);
+				}
+			}
+		}
 	}
 }
 #pragma endregion
