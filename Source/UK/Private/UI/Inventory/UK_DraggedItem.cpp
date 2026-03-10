023bd912e89d7dff122658ed61d224dcf567eac8
Give291 <whdydglgk@gmail.com>
2026-03-10 19:18:49 +0900

    03/10 Inv

---
+++ Source/UK/Private/UI/Inventory/UK_DraggedItem.cpp
@@ -0,0 +1,10 @@
+﻿#include "UI/Inventory/UK_DraggedItem.h"
+#include "Components/Image.h"
+
+void UUK_DraggedItem::SetIcon(UTexture2D* IconTexture)
+{
+	if ( ItemIcon && IconTexture )
+	{
+		ItemIcon->SetBrushFromTexture(IconTexture);
+	}
+}
