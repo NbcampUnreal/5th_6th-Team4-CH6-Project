73d5bd35b470970f16c97098df40074bdea24da1
Give291 <whdydglgk@gmail.com>
2026-03-11 15:28:47 +0900

    03/11 인벤토리 드래그

--- Source/UK/Private/UI/Inventory/UK_DragEquipSlot.cpp
+++ Source/UK/Private/UI/Inventory/UK_DragEquipSlot.cpp
@@ -1,5 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "UI/Inventory/UK_DragEquipSlot.h"
+#include "UI/Inventory/UK_InvDragDropOperation.h"
+#include "Components/Image.h"
+#include "DataAsset/Data/UK_ItemData.h"
 
+#include "Character/Weapon/UK_WeaponBase.h"
 
-#include "UI/Inventory/UK_DragEquipSlot.h"
+bool UUK_DragEquipSlot::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+	UE_LOG(LogTemp, Warning, TEXT("INV : Drop Detected")); //지울거
 
+	Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+
+	UUK_InvDragDropOperation* DragOp = Cast<UUK_InvDragDropOperation>(InOperation);
+
+	if (!DragOp) return false;
+	if (DragOp->DraggedSlotData.isEmpty()) return false;
+	
+
+	UE_LOG(LogTemp, Warning, TEXT("INV : Equip ItemID %s"), *DragOp->DraggedSlotData.ItemID.ToString());//지울거
+
+	return true;
+}
