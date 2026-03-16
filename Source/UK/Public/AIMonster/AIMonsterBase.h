5d3fd61d9f2510cc58b880c45a96db2587fdce6a
p00-h <149079533+p00-h@users.noreply.github.com>
2026-03-16 15:33:18 +0900

    Update 스포너 트리거 및 몬스터

--- Source/UK/Public/AIMonster/AIMonsterBase.h
+++ Source/UK/Public/AIMonster/AIMonsterBase.h
@@ -423,9 +423,6 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Monster|Data")
 	float CalculateExp(int32 PlayerLevel) const;
 	
-	UFUNCTION(BlueprintPure, Category = "Monster|Data")
-	float CalculateGold(int32 PlayerLevel) const;
-	
 private:
 	bool bRewardGranted = false;
 	FName GetRowName() const;
