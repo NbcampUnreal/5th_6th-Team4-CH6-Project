f947527c9dd1b68343f8b07b59af7658def088fc
chomuhyun <156655809+chomuhyun@users.noreply.github.com>
2026-03-18 21:22:18 +0900

    Add
    인게임 데이터 저장 관련 로직 구현중

---
+++ Source/UK/Public/Systems/Data/UK_InGameSave.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "UK_InGameSave.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UK_API UUK_InGameSave : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
