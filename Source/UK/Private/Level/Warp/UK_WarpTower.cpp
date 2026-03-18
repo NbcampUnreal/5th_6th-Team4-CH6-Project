c4cbcec35bbb72f9aef484ad53731b7873fd56cc
BK <quannie70@gmail.com>
2026-03-18 17:17:37 +0900

    3/18 워프타워 (오버랩시 체력, 마나 회복만구현)

---
+++ Source/UK/Private/Level/Warp/UK_WarpTower.cpp
@@ -0,0 +1,63 @@
+﻿#include "Level/Warp/UK_WarpTower.h"
+#include "Components/SphereComponent.h"
+#include "Character/UK_CharacterBase.h" 
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "Character/AttibuteSet/UK_PlayerStatusAttributeSet.h"
+
+AUK_WarpTower::AUK_WarpTower()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+	SetRootComponent(CollisionSphere);
+	CollisionSphere->SetSphereRadius(200.f);
+	CollisionSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CollisionSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	TowerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TowerMesh"));
+	TowerMesh->SetupAttachment(RootComponent);
+}
+
+void AUK_WarpTower::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 델리게이트 바인딩
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AUK_WarpTower::OnOverlapBegin);
+}
+
+void AUK_WarpTower::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 캐릭터인지 확인
+	AUK_CharacterBase* TargetCharacter = Cast<AUK_CharacterBase>(OtherActor);
+	if ( !TargetCharacter ) return;
+
+	// 최초 활성화 로직
+	if ( !bIsActivated )
+	{
+		bIsActivated = true;
+
+		// 블루프린트에서 이펙트나 사운드를 처리할 수 있게 호출
+		OnTowerActivated();
+
+		UE_LOG(LogTemp, Warning, TEXT("Warp Tower Activated!"));
+
+	}
+
+	// 체력/마나 회복 로직 
+	if ( UAbilitySystemComponent* ASC = TargetCharacter->GetAbilitySystemComponent() )
+	{
+		// 최대체력, 마나 가져오기
+		const float MaxHP = ASC->GetNumericAttribute(UUK_PlayerStatusAttributeSet::GetMaxHealthAttribute());
+		const float MaxMP = ASC->GetNumericAttribute(UUK_PlayerStatusAttributeSet::GetMaxMpAttribute());
+
+		// 현재 값을 최대치로 즉시 변경
+		ASC->SetNumericAttributeBase(UUK_PlayerStatusAttributeSet::GetHealthAttribute(), MaxHP);
+		ASC->SetNumericAttributeBase(UUK_PlayerStatusAttributeSet::GetCurrentMpAttribute(), MaxMP);
+
+		UE_LOG(LogTemp, Log, TEXT("WarpTower: Full Recovery Success (HP: %.f, MP: %.f)"), MaxHP, MaxMP);
+	}
+}
