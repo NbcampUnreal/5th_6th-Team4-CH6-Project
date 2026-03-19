6e9898b7b8a820ad9dc7f4e592fb7518dc8efc7e
ParkChanYeong <sidsru21131210@gmail.com>
2026-03-19 19:16:29 +0900

    캐릭터 무기 공격력 적용스테미너 소모 회복 적용

--- Source/UK/Public/Character/UK_CharacterBase.h
+++ Source/UK/Public/Character/UK_CharacterBase.h
@@ -100,6 +100,9 @@ protected:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	float GetFloorDistance();
+
+	UFUNCTION()
+	void HealStamina();
 	//geter, seter
 public:
 	TObjectPtr<USkeletalMeshComponent> GetRightHandWeapon() { return RightHandWeaponComponent; }
@@ -136,6 +139,10 @@ protected:
 #pragma endregion
 
 public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> HealStaminaEffect;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FActiveGameplayEffectHandle HealStaminaEffectHandle;
 	UPROPERTY(editAnywhere, BlueprintReadOnly, Category = "Sound")
 	TObjectPtr<USoundAttenuation> Attenuation;
 
@@ -160,6 +167,8 @@ public:
 	UPROPERTY(EditAnywhere)
 	UHitMontageDataAsset* HitMontageDataAsset;
 	
+	FTimerHandle StaminaHealTimerHandle;
+	
 	UPROPERTY(BlueprintReadWrite)
 	bool bInWater = false;	
 	UPROPERTY(BlueprintReadWrite)
@@ -167,6 +176,7 @@ public:
 	UPROPERTY(BlueprintReadWrite)
 	bool bIsGliding = false;
 	
+	bool bInUseStamina = false;
 #pragma endregion
 
 #pragma  region SaveGame
@@ -330,12 +340,15 @@ protected:
 	bool bIsCrouched;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool bIsSprinted;
+	bool bIsSprinted = false;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float SprintSpeed;
 
 
+	FTimerHandle GlidingTimer;
+	
+	FTimerHandle SprintTimer;
 	
 
 #pragma endregion
