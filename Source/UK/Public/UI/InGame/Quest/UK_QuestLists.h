06223cfb2d723b8b115ea463486b58e5ba2ddad7
Give291 <whdydglgk@gmail.com>
2026-03-19 20:42:03 +0900

    03/19 퀘스트 텍스트 연결

--- Source/UK/Public/UI/InGame/Quest/UK_QuestLists.h
+++ Source/UK/Public/UI/InGame/Quest/UK_QuestLists.h
@@ -1,6 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
@@ -8,9 +6,7 @@
 
 class UScrollBox;
 class UUK_QuestItem;
-/**
- * 
- */
+
 UCLASS()
 class UK_API UUK_QuestLists : public UUserWidget
 {
