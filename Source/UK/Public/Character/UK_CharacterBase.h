10754c3c132c2e35dad9eef80bc30a0f3f1b248a
haezzi2957 <haezzi2957@gmail.com>
2026-03-21 17:24:23 +0900

    캐릭터 레벨업

--- Source/UK/Public/Character/UK_CharacterBase.h
+++ Source/UK/Public/Character/UK_CharacterBase.h
@@ -32,6 +32,7 @@ class UUK_InputConfig;
 class UUK_InteractionComponent;
 class UUK_QuestComponent;
 class USoundAttenuation;
+class UNiagaraSystem;
 struct FInputActionValue;
 struct FUK_WeaponItemData;
 #pragma endregion
@@ -459,6 +460,16 @@ public:
 	void EndBattle();
 
 protected:
+	// 레벨업 시 재생할 나이아가라 이펙트
+	UPROPERTY(EditAnywhere, Category = "UK|LevelUp")
+	TObjectPtr<UNiagaraSystem> LevelUpVFX;
+
+	// 레벨업 시 재생할 사운드
+	UPROPERTY(EditAnywhere, Category = "UK|LevelUp")
+	TObjectPtr<USoundBase> LevelUpSound;
+
+	// 레벨업 로직 함수
+	void HandleLevelUp(float NewLevel);
 
 #pragma endregion
 
