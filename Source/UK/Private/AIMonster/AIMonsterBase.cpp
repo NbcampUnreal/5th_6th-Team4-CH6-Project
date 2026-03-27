0151588f4ae719fb7d7f0b12e0a82214b07d324a
p00-h <149079533+p00-h@users.noreply.github.com>
2026-03-27 14:31:52 +0900

    Update 몬스터 스텟 수정

--- Source/UK/Private/AIMonster/AIMonsterBase.cpp
+++ Source/UK/Private/AIMonster/AIMonsterBase.cpp
@@ -1215,7 +1215,7 @@ float AAIMonsterBase::CalculateDefense(int32 PlayerLevel) const
 {
     const FUK_MonsterStatRow* Row = GetStatRow();
     if (!Row) return 0.f;
-	return (PlayerLevel * 5.f) + Row->BaseDefense;
+	return Row->BaseDefense;
 }
 
 // ────────────────────────────────────────────────
